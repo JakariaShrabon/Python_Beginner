015a847ea25d3d6492b35bf4a2079b3da358a631
Jakaria_Shrabon <133669270+JakariaShrabon@users.noreply.github.com>
2023-11-01 20:46:54 +0600

    Basic

---
+++ Functions.py
@@ -0,0 +1,14 @@
+#simple function without argument
+def Print():
+    print("I am jakaria ")
+
+Print()
+
+#Functions with argument
+
+def info(Name,age):
+    print(f"My name is : {Name}")
+    print("My age is : "+str(age))
+
+info("Jakaria",50)
+
